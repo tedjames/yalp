110d82e0e9b32d96ad9d8af19dca0391627862dd
Ted Werbel <werbel.ted@gmail.com>
2017-06-19 20:03:20 -0400

    created mapView w/ header and two buttons

--- src/index.js
+++ src/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TouchableOpacity, StatusBar } from 'react-native';
+import { MapView } from 'expo';
+import { SimpleLineIcons, FontAwesome } from '@expo/vector-icons';
+
+// Used to hide unneccsary warning message from MapView
+console.ignoredYellowBox = ['Warning: View.propTypes'];
 
 const App = () => (
-  <View style={styles.container}>
-    <Text>Open up main.js to start working on your app!</Text>
-  </View>
+  <MapView
+    provider="google"
+    style={{ flex: 1 }}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  >
+    <StatusBar />
+    <View style={styles.header}>
+      <TouchableOpacity style={styles.menuButton}>
+        <SimpleLineIcons name="menu" size={19} color="#333" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.menuButton}>
+        <FontAwesome name="bookmark-o" size={24} color="#333" style={{ backgroundColor: 'transparent' }} />
+      </TouchableOpacity>
+    </View>
+  </MapView>
 );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = {
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingTop: 15
   },
-});
+  menuButton: {
+    backgroundColor: 'transparent',
+    padding: 20,
+  },
+  faveButton: {
+    backgroundColor: 'transparent',
+    height: 45,
+    width: 45,
+    borderRadius: 70,
+  },
+};
 
 export default App;
