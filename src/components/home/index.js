63ff47d6b764eacb1b4e865fb2ffa1b0289154ef
Ted Werbel <werbel.ted@gmail.com>
2017-06-27 12:51:52 -0400

    removed homeMap component and moved MapView to Home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,35 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Octicons } from '@expo/vector-icons';
-import HomeMap from './homeMap';
+import { MapView } from 'expo';
+import mapStyle from './mapStyle';
+
+// Used to hide unneccsary warning message from MapView
+console.ignoredYellowBox = ['Warning: View.propTypes'];
 
 const styles = {
   container: {
-    width: '85%',
+    position: 'absolute',
     height: 60,
+    width: '80%',
     alignSelf: 'center',
     top: 60,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     backgroundColor: 'white'
   },
   searchText: {
     color: 'black'
+  },
+  divider: {
+    width: 1,
+    height: 35,
+    alignSelf: 'center',
+    backgroundColor: '#ccc'
   }
 };
 
-const Home = () => (
-  <HomeMap>
-    <View style={styles.container}>
+export default class Home extends Component {
+  render() {
+    return (
       <View style={{ flex: 1 }}>
-        <Text style={styles.searchText}>Search</Text>
+        <MapView
+          provider="google"
+          customMapStyle={mapStyle}
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+        <View style={styles.container}>
+          <View>
+            <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Octicons name="search" size={32} color="#333" />
+              <Text style={styles.searchText}>What to eat?</Text>
+            </TouchableOpacity>
+          </View>
+
+
+          <View style={styles.divider} />
+
+          <Octicons name="settings" size={32} color="#333" />
+        </View>
       </View>
-
-      <View style={{ width: 1, height: 35, alignSelf: 'center', backgroundColor: '#ccc' }} />
-
-      <Octicons name="settings" size={32} color="#333" />
-    </View>
-  </HomeMap>
-);
-
-export default Home;
+    );
+  }
+}
