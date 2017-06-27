d8709f80ae769e67d6ddb79c62a20693442d7316
Ted Werbel <werbel.ted@gmail.com>
2017-06-27 17:35:42 -0400

    added gradient fade behind shortcutBar

--- src/components/home/shortcutBar.js
+++ src/components/home/shortcutBar.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { LinearGradient } from 'expo';
 import Shortcut from './shortcut';
 
 const styles = {
-  bar: {
+  gradientBar: {
     position: 'absolute',
     flexDirection: 'row',
     alignSelf: 'center',
     justifyContent: 'space-between',
-    width: '70%',
-    bottom: 40,
-    paddingTop: 20,
-    paddingBottom: 20,
+    width: '100%',
+    bottom: 0,
+    paddingBottom: 40,
+    paddingTop: 5,
+    paddingRight: 60,
+    paddingLeft: 60
   }
 };
 
 export default class ShortcutBar extends Component {
   render() {
     return (
-      <View style={styles.bar}>
+      <LinearGradient style={styles.gradientBar} colors={['rgba(238, 238, 238, 0)', 'rgba(238, 238, 238, 0.925)']} locations={[0, 0.75]}>
         <Shortcut label="Breakfast" />
         <Shortcut label="Cafe" />
         <Shortcut label="Fast Food" />
-      </View>
+      </LinearGradient>
     );
   }
 }
