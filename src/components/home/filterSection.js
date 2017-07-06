bdb73adb50911ac65049a2b051d8ce9713e264f8
Ted Werbel <werbel.ted@gmail.com>
2017-07-06 16:27:05 -0400

    changed styling of and added animations to filterSection

--- src/components/home/filterSection.js
+++ src/components/home/filterSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Animated, Text } from 'react-native';
 import { Font } from 'expo';
 
 const styles = {
@@ -10,7 +10,7 @@ const styles = {
     letterSpacing: 0.2,
     marginLeft: 15,
     marginBottom: 2.5,
-    marginTop: 5
+    marginTop: 7.5
   },
 };
 
@@ -24,19 +24,28 @@ export default class FilterSection extends Component {
   }
   async componentDidMount() {
     await Font.loadAsync({
-      'open-sans-regular': require('../../../assets/fonts/OpenSans-Regular.ttf')
+      'open-sans-regular': require('../../../assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-semibold': require('../../../assets/fonts/OpenSans-SemiBold.ttf'),
+      'open-sans-light': require('../../../assets/fonts/OpenSans-Light.ttf'),
+      'open-sans-bold': require('../../../assets/fonts/OpenSans-Bold.ttf'),
+      'open-sans-extrabold': require('../../../assets/fonts/OpenSans-ExtraBold.ttf'),
+      'rubik-light': require('../../../assets/fonts/Rubik-Light.ttf'),
+      'rubik-medium': require('../../../assets/fonts/Rubik-Medium.ttf'),
+      'rubik-regular': require('../../../assets/fonts/Rubik-Regular.ttf'),
     });
     this.setState({ fontLoaded: true });
   }
   render() {
     const { fontLoaded } = this.state;
     const { name, children } = this.props;
+    const sectionPosition = { transform: [{ translateY: this.props.sectionPosition }] };
+    const sectionOpacity = { opacity: this.props.sectionOpacity };
 
     return (
-      <View>
+      <Animated.View style={[sectionPosition, sectionOpacity]}>
         {fontLoaded ? <Text style={styles.sectionHeader}>{name}</Text> : null}
         {children}
-      </View>
+      </Animated.View>
     );
   }
 }
