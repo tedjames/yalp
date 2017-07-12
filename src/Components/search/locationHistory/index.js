0199d1cb470e29f38a08d6352902211e5331f974
Ted Werbel <werbel.ted@gmail.com>
2017-07-11 20:08:08 -0400

    temporary locationHistory component

---
+++ src/Components/search/locationHistory/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class LocationHistory extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LocationHistory</Text>
+      </View>
+    );
+  }
+}
