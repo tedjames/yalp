6e33378846bff2d7cf955bcfdb5fb1261c8376d6
Ted Werbel <werbel.ted@gmail.com>
2017-07-18 16:29:20 -0400

    added searchQuery to searchBar and Header components

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, StatusBar } from 'react-native';
 import Shortcuts from './shortcuts';
 import SearchBar from './searchBar';
@@ -6,17 +7,24 @@ import SearchModal from '../search';
 import FilterModal from '../filter';
 import Maps from './maps';
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
+    const { searchQuery } = this.props;
     return (
       <View style={{ flex: 1 }}>
         <StatusBar hidden={false} animated />
         <FilterModal />
-        <SearchModal />
+        <SearchModal searchQuery={searchQuery} />
         <Maps />
-        <SearchBar />
+        <SearchBar searchQuery={searchQuery} />
         <Shortcuts />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  searchQuery: state.fields.searchQuery
+});
+
+export default connect(mapStateToProps)(Home);
