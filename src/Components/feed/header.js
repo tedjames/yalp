8e3ad63d7a77fa67cce19482d90c8a6752455c2f
Ted Werbel <werbel.ted@gmail.com>
2017-09-01 14:47:46 -0400

    added transition and modified interpolation/animations for arrow + text

--- src/Components/feed/header.js
+++ src/Components/feed/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Animated, Easing } from 'react-native';
 import Arrow from './arrow';
+import * as Animatable from 'react-native-animatable';
 
 export default class Header extends Component {
   constructor(props) {
@@ -13,14 +14,14 @@ export default class Header extends Component {
   render() {
     const { scrollY, sectionHeight } = this.props;
     const headerTextTop = scrollY.interpolate({
-      inputRange: [25, sectionHeight * 0.65],
+      inputRange: [10, sectionHeight * 0.65],
       outputRange: [85, 23],
       extrapolate: 'clamp',
       easing: Easing.ease.out
     });
     const headerTextLeft = scrollY.interpolate({
-      inputRange: [0, sectionHeight * 0.5],
-      outputRange: [15, 65],
+      inputRange: [0, sectionHeight * 0.3, sectionHeight * 0.65],
+      outputRange: [15, 50, 65],
       extrapolate: 'clamp',
       easing: Easing.ease.out
     });
@@ -37,7 +38,7 @@ export default class Header extends Component {
     });
     const arrowScale = scrollY.interpolate({
       inputRange: [0, sectionHeight * 0.5],
-      outputRange: [1, 0.85],
+      outputRange: [1, 0.825],
       extrapolate: 'clamp'
     });
     const arrowOpacity = scrollY.interpolate({
@@ -47,7 +48,7 @@ export default class Header extends Component {
     });
     const arrowRight = scrollY.interpolate({
       inputRange: [0, sectionHeight * 1],
-      outputRange: [0, 12],
+      outputRange: [0, 11],
       extrapolate: 'clamp'
     });
     const arrowTop = scrollY.interpolate({
@@ -69,92 +70,105 @@ export default class Header extends Component {
     const headerOpacity = scrollY.interpolate({
       inputRange: [0, sectionHeight * 1.25],
       outputRange: [1, 0],
-      extrapolate: 'clamp'
+      extrapolate: 'clamp',
+      useNativeDriver: true
     });
     const headerOpacity2 = scrollY.interpolate({
       inputRange: [sectionHeight / 1.25, sectionHeight, sectionHeight * 2],
       outputRange: [0, 1, 0],
-      extrapolate: 'clamp'
+      extrapolate: 'clamp',
+      useNativeDriver: true
     });
     const headerOpacity3 = scrollY.interpolate({
       inputRange: [(sectionHeight * 2) / 1.25, sectionHeight * 2, sectionHeight * 3],
       outputRange: [0, 1, 0],
-      extrapolate: 'clamp'
+      extrapolate: 'clamp',
+      useNativeDriver: true
     });
     const headerOpacity4 = scrollY.interpolate({
       inputRange: [(sectionHeight * 3) / 1.25, sectionHeight * 3, sectionHeight * 4],
       outputRange: [0, 1, 0],
-      extrapolate: 'clamp'
+      extrapolate: 'clamp',
+      useNativeDriver: true
     });
     const headerOpacity5 = scrollY.interpolate({
       inputRange: [(sectionHeight * 4) / 1.25, sectionHeight * 4],
       outputRange: [0, 1],
-      extrapolate: 'clamp'
+      extrapolate: 'clamp',
+      useNativeDriver: true
     });
     return (
-      <Animated.View style={[styles.header, { height: headerHeight }]}>
-        <Arrow arrowRight={arrowRight} arrowScale={arrowScale} arrowRotate={arrowRotate} arrowTop={arrowTop} arrowOpacity={arrowOpacity} />
-        <Animated.Text
-          style={[styles.headerText,
-            {
-              top: headerTextTop,
-              left: headerTextLeft,
-              color: headerTextColor,
-              fontSize: headerFontSize,
-              opacity: headerOpacity
-            }]}
-        >
-          Popular near you
-        </Animated.Text>
-        <Animated.Text
-          style={[styles.headerText,
-            {
-              top: headerTextTop,
-              left: headerTextLeft,
-              color: headerTextColor,
-              fontSize: headerFontSize,
-              opacity: headerOpacity2
-            }]}
-        >
-          Recommended
-        </Animated.Text>
-        <Animated.Text
-          style={[styles.headerText,
-            {
-              top: headerTextTop,
-              left: headerTextLeft,
-              color: headerTextColor,
-              fontSize: headerFontSize,
-              opacity: headerOpacity3
-            }]}
-        >
-          Delivery near you
-        </Animated.Text>
-        <Animated.Text
-          style={[styles.headerText,
-            {
-              top: headerTextTop,
-              left: headerTextLeft,
-              color: headerTextColor,
-              fontSize: headerFontSize,
-              opacity: headerOpacity4
-            }]}
-        >
-          Bookmarks
-        </Animated.Text>
-        <Animated.Text
-          style={[styles.headerText,
-            {
-              top: headerTextTop,
-              left: headerTextLeft,
-              color: headerTextColor,
-              fontSize: headerFontSize,
-              opacity: headerOpacity5
-            }]}
-        >
-          Top Categories
-        </Animated.Text>
-      </Animated.View>
+      <Animatable.View animation="fadeInUp" duration={1250} style={{ zIndex: 5 }}>
+        <Animated.View style={[styles.header, { height: headerHeight }]}>
+          <Arrow
+            arrowRight={arrowRight}
+            arrowScale={arrowScale}
+            arrowRotate={arrowRotate}
+            arrowTop={arrowTop}
+            arrowOpacity={arrowOpacity}
+          />
+          <Animated.Text
+            style={[styles.headerText,
+              {
+                top: headerTextTop,
+                left: headerTextLeft,
+                color: headerTextColor,
+                fontSize: headerFontSize,
+                opacity: headerOpacity
+              }]}
+          >
+            Popular near you
+          </Animated.Text>
+          <Animated.Text
+            style={[styles.headerText,
+              {
+                top: headerTextTop,
+                left: headerTextLeft,
+                color: headerTextColor,
+                fontSize: headerFontSize,
+                opacity: headerOpacity2
+              }]}
+          >
+            Recommended
+          </Animated.Text>
+          <Animated.Text
+            style={[styles.headerText,
+              {
+                top: headerTextTop,
+                left: headerTextLeft,
+                color: headerTextColor,
+                fontSize: headerFontSize,
+                opacity: headerOpacity3
+              }]}
+          >
+            Delivery near you
+          </Animated.Text>
+          <Animated.Text
+            style={[styles.headerText,
+              {
+                top: headerTextTop,
+                left: headerTextLeft,
+                color: headerTextColor,
+                fontSize: headerFontSize,
+                opacity: headerOpacity4
+              }]}
+          >
+            Bookmarks
+          </Animated.Text>
+          <Animated.Text
+            style={[styles.headerText,
+              {
+                top: headerTextTop,
+                left: headerTextLeft,
+                color: headerTextColor,
+                fontSize: headerFontSize,
+                opacity: headerOpacity5
+              }]}
+          >
+            Top Categories
+          </Animated.Text>
+        </Animated.View>
+      </Animatable.View>
     );
   }
 }
